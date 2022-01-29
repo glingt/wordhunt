dc334dc28039754a4fd94f9b1e12fdc660bb968a
Arvid Kästel <arvid.kastel@gmail.com>
2022-01-29 20:43:23 +0100

    font

--- remix/app/root.tsx
+++ remix/app/root.tsx
@@ -17,6 +17,9 @@ export default function App() {
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&display=swap" rel="stylesheet">  </link>
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <Meta />
         <Links />
--- remix/app/routes/index.tsx
+++ remix/app/routes/index.tsx
@@ -1,6 +1,6 @@
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+    <div style={{ fontFamily: "Montserrat, sans-serif", lineHeight: "1.4" }}>
       Wordhunt
     </div>
   );
